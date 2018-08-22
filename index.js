1abfdb72350b5f1b28f7651b0b3db41df1db461a
iamsostokes <iamsostokes@gmail.com>
2018-08-22 19:24:32 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+
+
+
+
+$('#aboutButton').click(function(){
+    $('.MenuBody').show();
+    $('.MenuBody2').hide();
+    $('.MenuBody3').hide();
+   
+})
+
+$('#skillsButton').click(function(){
+    $('.MenuBody2').show();
+    $('.MenuBody').hide();
+    $('.MenuBody3').hide();
+   
+})
+
+$('#equipmentButton').click(function(){
+    $('.MenuBody3').show();
+    $('.MenuBody').hide();
+    $('.MenuBody2').hide();
+   
+})
